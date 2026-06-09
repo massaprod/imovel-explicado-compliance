03572f1aba1a606fa13a24d4d64b31e914d73347
Luis Carlos <imovelexplicado@gmail.com>
2026-06-09 09:31:08 -0300

    Redirect contract subdomain root with Pages middleware

---
+++ functions/_middleware.js
@@ -0,0 +1,10 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  if (url.hostname === "contratos.imovelexplicado.com.br" && url.pathname === "/") {
+    url.pathname = "/contratos";
+    return Response.redirect(url.toString(), 302);
+  }
+
+  return context.next();
+}
